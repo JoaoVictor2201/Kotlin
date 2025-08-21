6cfd0ac222f42607ebc95b6003406a6d0f073f93
Joao Victor <franca.jv@hotmail.com>
2025-08-20 22:32:25 -0300

    Exercicios - Ex09

---
+++ src/ex09.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("===== Conversor de metros =====")
+
+    print("Informe uma distância em metros: ")
+    val metros = readln().toDouble()
+
+    println("Distância de ${metros * 100} centimetros")
+}
