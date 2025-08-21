35690d82d9e20ac985af1605d71fcd28ac5796a6
Joao Victor <franca.jv@hotmail.com>
2025-08-20 23:03:46 -0300

    Exercicios - Ex13

---
+++ src/ex13.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("===== Calculo de desconto =====")
+    print("Valor original do produto: ")
+    val valor = readln().toFloat()
+
+    println("O valor com desconto de 5% é de ${valor * 0.95}")
+}
